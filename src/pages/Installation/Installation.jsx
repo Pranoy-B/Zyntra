9b5c79d3be7af4bb0f8ce03b2c87de6c14c5bc6d
Pranoy-B <pranoybarua0012@gmail.com>
2025-10-16 00:39:22 +0600

    redirects file added

--- src/pages/Installation/Installation.jsx
+++ src/pages/Installation/Installation.jsx
@@ -7,7 +7,7 @@ import { ToastContainer, toast } from "react-toastify";
 const Installation = () => {
   const [installed, setInstalled] = use(AppContext);
   const notify = () => toast("App Uninstalled!");
-  const [sort,setSort] = useState('')
+  const [sort, setSort] = useState("");
 
   const handleUnInstall = (app) => {
     const filteredApps = installed.filter(
@@ -15,32 +15,38 @@ const Installation = () => {
     );
     setInstalled(filteredApps);
   };
-  const handleSort = (type) =>{
-    
-        setSort(type);
-        if (type==="downloads"){
-          const sortedByDownloads = [...installed].sort((a,b) => a.downloads-b.downloads);
-          setInstalled(sortedByDownloads)
-        }
-  }
+  const handleSort = (type) => {
+    setSort(type);
+    if (type === "downloads") {
+      const sortedByDownloads = [...installed].sort(
+        (a, b) => a.downloads - b.downloads
+      );
+      setInstalled(sortedByDownloads);
+    }
+  };
 
   return (
     <div className="max-w-[1280px] mx-auto text-center p-8 min-h-[500px] space-y-5">
       <h1 className="font-bold text-2xl flex-start">
         Your installed Apps are here
       </h1>
-      <div className="dropdown">
-        <div tabIndex={0} role="button" className="btn m-1">
-          Sort
+      <div className="flex justify-between items-center">
+        <div>
+          <h3>{installed.length} apps found</h3>
         </div>
-        <ul
-          tabIndex="-1"
-          className="dropdown-content menu bg-base-100 rounded-box z-1 w-52 p-2 shadow-sm"
-        >
-          <li>
-            <a onClick={()=>handleSort("downloads")}>By Downloads</a>
-          </li>
-        </ul>
+        <div className="dropdown">
+          <div tabIndex={0} role="button" className="btn m-1">
+            Sort
+          </div>
+          <ul
+            tabIndex="-1"
+            className="dropdown-content menu bg-base-100 rounded-box z-1 w-52 p-2 shadow-sm"
+          >
+            <li>
+              <a onClick={() => handleSort("downloads")}>By Downloads</a>
+            </li>
+          </ul>
+        </div>
       </div>
       <div>
         {installed.length === 0 ? (
@@ -80,14 +86,15 @@ const Installation = () => {
                   </div>
 
                   <button
-                    onClick={() => {handleUnInstall(app); notify()}}
+                    onClick={() => {
+                      handleUnInstall(app);
+                      notify();
+                    }}
                     className="h-[40px] p-2 border-2 border-gray-200 bg-white rounded-md font-bold text-[#074fad]"
                   >
                     Uninstall
                   </button>
-                 
                 </div>
-                 
               </div>
             ))}
           </div>
